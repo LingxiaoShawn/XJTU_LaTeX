38ed5d16b4b661776111e16d0e5f7628a3bb7890
jmx <jmx@debian8-3>
2016-06-03 12:12:31 +0000

    update help message

--- utility/Chinese_Character_Counter_linux_mac.sh
+++ utility/Chinese_Character_Counter_linux_mac.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-#########################################################
-# Author    : J.Mx                                      #
-# Homepage  : id9502.github.io                          #
-# Date      : 2016-06-03                                #
-#                                                       #
-# Usage     : run this script inside the folder which   #
-#             contains your *.tex file                  #
-#                                                       #
-#             to make it executable, use command:       #
-#             chmod +x <this/name>.sh                   #
-#########################################################
+#####################################################################################
+# Author    : J.Mx                                                                  #
+# Homepage  : id9502.github.io                                                      #
+# Date      : 2016-06-03                                                            #
+#                                                                                   #
+# Usage     : run this script inside the folder which contains your *.tex file      #
+#                                                                                   #
+#             to make it executable, use command: chmod +x <this/name>.sh           #
+#                                                                                   #
+#             for getting the result, run this script in a shell window like bash,  #
+#             instead of click it directly                                          #
+#####################################################################################
 
 printf "Chinese characters count (with punctuation):\n\n"
 for path in *.tex
